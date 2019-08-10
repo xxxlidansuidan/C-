4a0509748a0998755cc0ba7a1c044fe2f01e7272
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-08-10 23:28:48 +0600

    Update CountRandomLettersInArray.cpp

--- CountRandomLettersInArray.cpp
+++ CountRandomLettersInArray.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
 const size_t arraySize{100};
-const 	size_t numberOfLetters{26};
+const size_t numberOfLetters{26};
+
 void creatArray(char []);
 void displayArray(const char []);
 void countLetters(const char [], int []);
 void displayCounts(const int []);
+
 int main() {
 	char ch[arraySize];
 	creatArray(ch);
@@ -18,22 +21,26 @@ int main() {
 	displayCounts(counts);
 	cout << endl;
 }
+
 void creatArray(char ch[]) {
 	srand(time(0));
 	for (unsigned int i{0}; i < arraySize; ++i) {
 		ch[i] = static_cast<char>('a' + rand() % ('z' - 'a' + 1));
 	}
 }
+
 void displayArray(const char ch[]) {
 	for (unsigned int i{0}; i < arraySize; ++i) {
 		cout << ch[i] << ((i + 1) % 20 == 0 ? "\n" : " ");
 	}
 }
+
 void countLetters(const char ch[], int counts[]) {
 	for (unsigned int i{0}; i < arraySize; ++i) {
 		++counts[ch[i] - 'a'];
 	}
 }
+
 void displayCounts(const int counts[]) {
 	for (unsigned int i{0}; i < numberOfLetters; ++i) {
 		cout << "(" << counts[i] << static_cast<char>('a' + i) << ")" << ((i + 1) % 10 == 0 ? "\n" : " ");
